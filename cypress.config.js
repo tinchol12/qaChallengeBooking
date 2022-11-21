57ea1b506e866a294d11396e1e9ffd2552b572ba
Martin Osses <50757354+tinchol12@users.noreply.github.com>
2022-11-21 15:09:09 +0100

    Update 1.1
    push

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  chromeWebSecurity: false,
+  defaultCommandTimeout: 8000,
+  video: false,
+  screenshotOnRunFailure: true,  
+  pageLoadTimeout: 30000,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
